b3643e0db5a61024c6cb098d30a3542ffb0f8221
Seth Eliot <seliot@amazon.com>
2020-01-31 21:33:48 -0800

    add custom resource to populate DDB table

--- Reliability/300_Health_Checks_and_Dependencies/Code/Python/load_ddb.py
+++ Reliability/300_Health_Checks_and_Dependencies/Code/Python/load_ddb.py
@@ -1,17 +1,70 @@
-import json
-from ec2_metadata import ec2_metadata
-
-# For Database
 import boto3
+import urllib.request
+import cfnresponse
+import logging
+import signal
+import json
+import traceback
 
-# Get the service resource. @TODO use EC2 metadata (ec2_metadata.region)
+LOGGER = logging.getLogger()
+LOGGER.setLevel(logging.INFO)
+
+def populate_ddb_table():
+    # Get the service resource. @TODO use EC2 metadata (ec2_metadata.region)
+    LOGGER.info('creat client')
+    client = boto3.client('dynamodb', 'us-east-2')
+    
+    # @TODO, make bucket/object configurable
+    # file = open('../Data/serviceCallMocks.json')
+    LOGGER.info('open file')
+    file = urllib.request.urlopen("https://aws-well-architected-labs-ohio.s3.us-east-2.amazonaws.com/Healthcheck/Data/serviceCallMocks.json")
+    LOGGER.info('read file')
+    text = file.read()
+    LOGGER.info(text)
+    LOGGER.info('convert to JSON')
+    request_items = json.loads(text)
+    LOGGER.info('write to DDB')
+    response = client.batch_write_item(RequestItems=request_items)
+    LOGGER.info('DONE')
+    
+    # @TODO add error handling 
+    
+    # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dynamodb.html#DynamoDB.Client.batch_write_item
 
 
-client = boto3.client('dynamodb', 'us-east-2')
+def handler(event, context):
+    signal.alarm(int((context.get_remaining_time_in_millis() / 1000) - 1))
+    # Setup alarm for remaining runtime minus a second
+    try:
+        LOGGER.info('EVENT RECEIVED: %s', event)
+        LOGGER.info('CONTEXT RECEIVED: %s', context)
+        if event['RequestType'] == 'Create':
+            LOGGER.info('CREATE')
+            populate_ddb_table()
+            cfnresponse.send(event, context, cfnresponse.SUCCESS,
+                {'Message': 'Resource creation successful!'})
+        elif event['RequestType'] == 'Update':
+            LOGGER.info('UPDATE')
+            populate_ddb_table()
+            cfnresponse.send(event, context, cfnresponse.SUCCESS,
+                {'Message': 'Resource update successful!'})
+        elif event['RequestType'] == 'Delete':
+            LOGGER.info('DELETE')
+            # do nothing, table will be deleted anyway
+            cfnresponse.send(event, context, cfnresponse.SUCCESS,
+                {'Message': 'Resource deletion successful!'})
+        else:
+            LOGGER.info('FAILED!')
+            cfnresponse.send(event, context, cfnresponse.FAILED,
+                {'Message': 'Unexpected event received from CloudFormation'})
+    except Exception as e:
+        LOGGER.info(str(traceback.format_exception_only(e.__class__, e)))
+        LOGGER.info('FAILED!')
+        cfnresponse.send(event, context, cfnresponse.FAILED, {
+            'Message': 'Exception during processing'})
 
-# @TODO, read file from S3 or github
-file = open('../Data/serviceCallMocks.json')
-request_items = json.loads(file.read())
-response = client.batch_write_item(RequestItems=request_items)
+def timeout_handler(_signal, _frame):
+    raise Exception('Time exceeded')
 
-# @TODO add error handling 
+
+signal.signal(signal.SIGALRM, timeout_handler)
