9c21100dd57b7741de32cfe2066106d0fbadca73
TheFuz <mahanth.jayadev@gmail.com>
2021-01-27 18:23:53 -0600

    Updating REL 200 Test Backup and Restore Lambda code to Python 3 (#224)
    * Updating REL 200 Test Backup and Restore Lambda code to Python 3
    
    * Removed extra print from commit 45ac180134358141f6f2377684726043437adea7
    
    Co-authored-by: TheFuz <jayadeva@a45e60be5cef.ant.amazon.com>

--- static/Reliability/200_Testing_Backup_and_Restore_of_Data/Code/lambda_function.py
+++ static/Reliability/200_Testing_Backup_and_Restore_of_Data/Code/lambda_function.py
@@ -5,20 +5,21 @@ import boto3
 backup = boto3.client('backup')
 
 def lambda_handler(event, context):
-    print 'Incoming Event:' + json.dumps(event)
+    print('Incoming Event:' + json.dumps(event))
 
     try:
         if event['Records'][0]['Sns']['Subject'] == 'Restore Test Status':
-            print 'No action required, deletion of new resource confirmed.'
+            print('No action required, deletion of new resource confirmed.')
+            return
     except Exception as e:
-            print str(e)
+            print(str(e))
             return
 
     job_type = event['Records'][0]['Sns']['Message'].split('.')[-1].split(' ')[1]
 
     try:
         if 'failed' in event['Records'][0]['Sns']['Message']:
-            print 'Something has failed. Please review the job in the AWS Backup console.'
+            print('Something has failed. Please review the job in the AWS Backup console.')
             return 'Job ID:' + event['Records'][0]['Sns']['Message'].split('.')[-1].split(':')[1].strip()
         elif job_type == 'Backup':
             backup_job_id = event['Records'][0]['Sns']['Message'].split('.')[-1].split(':')[1].strip()
@@ -58,14 +59,14 @@ def lambda_handler(event, context):
                 metadata['RestoreMetadata']['CreationToken'] = metadata['RestoreMetadata']['file-system-id'] + '-restore-test'
 
             #API call to start the restore job
-            print 'Starting the restore job'
+            print('Starting the restore job')
             restore_request = backup.start_restore_job(
                     RecoveryPointArn=recovery_point_arn,
                     IamRoleArn=iam_role_arn,
                     Metadata=metadata['RestoreMetadata']
             )
 
-            print json.dumps(restore_request)
+            print(json.dumps(restore_request))
 
             return
         elif job_type == 'Restore':
@@ -76,27 +77,27 @@ def lambda_handler(event, context):
                         )
             resource_type = restore_info['CreatedResourceArn'].split(':')[2]
 
-            print 'Restore from the backup was successful. Deleting the newly created resource.'
+            print('Restore from the backup was successful. Deleting the newly created resource.')
 
             #determine resource type that was restored and delete it to save cost
             if resource_type == 'dynamodb':
                 dynamo = boto3.client('dynamodb')
                 table_name = restore_info['CreatedResourceArn'].split(':')[5].split('/')[1]
-                print 'Deleting: ' + table_name
+                print('Deleting: ' + table_name)
                 delete_request = dynamo.delete_table(
                                     TableName=table_name
                                 )
             elif resource_type == 'ec2':
                 ec2 = boto3.client('ec2')
                 volume_id = restore_info['CreatedResourceArn'].split(':')[5].split('/')[1]
-                print 'Deleting: ' + volume_id
+                print('Deleting: ' + volume_id)
                 delete_request = ec2.delete_volume(
                             VolumeId=volume_id
                         )
             elif resource_type == 'rds':
                 rds = boto3.client('rds')
                 database_identifier = restore_info['CreatedResourceArn'].split(':')[6]
-                print 'Deleting: ' + database_identifier
+                print('Deleting: ' + database_identifier)
                 delete_request = rds.delete_db_instance(
                             DBInstanceIdentifier=database_identifier,
                             SkipFinalSnapshot=True
@@ -104,14 +105,14 @@ def lambda_handler(event, context):
             elif resource_type == 'elasticfilesystem':
                 efs = boto3.client('efs')
                 elastic_file_system = restore_info['CreatedResourceArn'].split(':')[5].split('/')[1]
-                print 'Deleting: ' + elastic_file_system
+                print('Deleting: ' + elastic_file_system)
                 delete_request = efs.delete_file_system(
                             FileSystemId=elastic_file_system
                         )
 
             sns = boto3.client('sns')
 
-            print 'Sending deletion confirmation'
+            print('Sending deletion confirmation')
             #send a final notification confirming deletion of the newly restored resource
             notify = sns.publish(
                 TopicArn=topic_arn,
@@ -119,10 +120,10 @@ def lambda_handler(event, context):
                 Subject='Restore Test Status'
             )
 
-            print json.dumps(notify)
+            print(json.dumps(notify))
 
-            print json.dumps(delete_request)
+            print(json.dumps(delete_request))
             return
     except Exception as e:
-        print str(e)
+        print(str(e))
         return
