86fae97653b7409f701f63496432c80e4d8840ee
awssteph <awssteph@amazon.com>
2021-04-25 16:37:03 +0100

    gets account and ou and upload

--- static/Cost/300_Organization_Data_CUR_Connection/Code/org_data_ou.py
+++ static/Cost/300_Organization_Data_CUR_Connection/Code/org_data_ou.py
@@ -30,30 +30,37 @@ def lambda_handler(event, context):
     root_id    = client.list_roots()['Roots'][0]['Id']
     ou_id_list = get_ou_ids(root_id, 'ORGANIZATIONAL_UNIT', client)
     
-    with open('/tmp/org.csv', 'w') as f: # Saving in the temporay folder in the lambda
+    with open('/tmp/ou-org.csv', 'w') as f: # Saving in the temporay folder in the lambda
         for ou in ou_id_list:
+            account_data(f, ou, client)
+    s3_upload('ou-org')
 
-            account_id_list = get_ou_ids(ou, 'ACCOUNT', client)
-            for account_id in account_id_list:
-                response = client.describe_account(AccountId=account_id)
-                aid = response["Account"]["Id"]
-                name = response["Account"]["Name"]
-                time = response["Account"]["JoinedTimestamp"]
-                status = response["Account"]["Status"]
+    with open('/tmp/acc-org.csv', 'w') as f: # Saving in the temporay folder in the lambda
+        account_data(f, root_id, client)
+    s3_upload('acc-org')
 
-                line = f"{aid}, {name}, {time}, {status}, {ou}\n"
-                import pdb; pdb.set_trace()
-                f.write(line)
-            print("respose gathered")
+def account_data(f, parent, client):
+    account_id_list = get_ou_ids(parent, 'ACCOUNT', client)
+    for account_id in account_id_list:
+        response = client.describe_account(AccountId=account_id)
+        aid = response["Account"]["Id"]
+        name = response["Account"]["Name"]
+        time = response["Account"]["JoinedTimestamp"]
+        status = response["Account"]["Status"]
+        email = response["Account"]["Email"]
 
-def s3_upload():
+        line = f"{aid}, {name}, {time}, {status},{email}, None\n"
+        
+        f.write(line)
+
+def s3_upload(file_name):
     bucket = os.environ["BUCKET_NAME"] #Using environment variables below the Lambda will use your S3 bucket
     try:
         s3 = boto3.client('s3', 'eu-west-1',
                         config=Config(s3={'addressing_style': 'path'}))
         s3.upload_file(
-            '/tmp/org.csv', bucket, "organisation-data/org.csv") #uploading the file with the data to s3
-        print("org data in s3")
+            f'/tmp/{file_name}.csv', bucket, f"organisation-data/{file_name}.csv") #uploading the file with the data to s3
+        print(f"{file_name}org data in s3")
     except Exception as e:
         print(e)
 
