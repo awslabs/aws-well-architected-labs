f7b62e8cda496e26dec1615be5d0dc2e7743203b
awssteph <awssteph@amazon.com>
2021-04-25 16:18:30 +0100

    pulled ous

--- static/Cost/300_Organization_Data_CUR_Connection/Code/org_data.py
+++ static/Cost/300_Organization_Data_CUR_Connection/Code/org_data.py
@@ -1,65 +1,52 @@
 #!/usr/bin/env python3
-
+    
+import argparse
 import boto3
 from botocore.exceptions import ClientError
 from botocore.client import Config
 import os
-import json
-import datetime
-
-def myconverter(o):
-    if isinstance(o, datetime.datetime):
-        return o.__str__()
 
 def list_accounts():
-    bucket = os.environ["BUCKET_NAME"] #Using enviroment varibles below the lambda will use your S3 bucket
-    tags_check = os.environ["TAGS"]
+    bucket = os.environ["BUCKET_NAME"] #Using environment variables below the Lambda will use your S3 bucket
 
     sts_connection = boto3.client('sts')
     acct_b = sts_connection.assume_role(
         RoleArn="arn:aws:iam::(account id):role/OrganizationLambdaAccessRole",
         RoleSessionName="cross_acct_lambda"
     )
-        
+    
     ACCESS_KEY = acct_b['Credentials']['AccessKeyId']
     SECRET_KEY = acct_b['Credentials']['SecretAccessKey']
     SESSION_TOKEN = acct_b['Credentials']['SessionToken']
 
     # create service client using the assumed role credentials
     client = boto3.client(
-        "organizations", region_name="us-east-1", #Using the Organization client to get the data. This MUST be us-east-1 regardless of region you have the lamda in
+        "organizations", region_name="us-east-1", #Using the Organizations client to get the data. This MUST be us-east-1 regardless of region you have the Lamda in
         aws_access_key_id=ACCESS_KEY,
         aws_secret_access_key=SECRET_KEY,
         aws_session_token=SESSION_TOKEN,
     )
+
+    
     paginator = client.get_paginator("list_accounts") #Paginator for a large list of accounts
     response_iterator = paginator.paginate()
-    with open('/tmp/org.json', 'w') as f: # Saving in the temporay folder in the lambda
+    with open('/tmp/org.csv', 'w') as f: # Saving in the temporay folder in the lambda
 
         for response in response_iterator: # extracts the needed info
             for account in response["Accounts"]:
-                aid = account["Id"]                
-                if tags_check != '':
-                    tags_list = client.list_tags_for_resource(ResourceId=aid) #gets the lists of tags for this account
-                    
-                    for tag in os.environ.get("TAGS").split(","): #looking at tags in the enviroment variables split by a space
-                        for org_tag in tags_list['Tags']:
-                            if tag == org_tag['Key']: #if the tag found on the account is the same as the current one in the environent varibles, add it to the data
-                                value = org_tag['Value']
-                                kv = {tag : value}
-                                account.update(kv)
-                        
-                data = json.dumps(account, default = myconverter) #converts datetime to be able to placed in json
-
-                f.write(data)
-                f.write('\n')
+                aid = account["Id"]
+                name = account["Name"]
+                time = account["JoinedTimestamp"]
+                status = account["Status"]
+                line = "%s, %s, %s, %s\n" % (aid, name, time, status)
+                f.write(line)
     print("respose gathered")
 
     try:
-        s3 = boto3.client('s3', 'eu-west-1',
+        s3 = boto3.client('s3', '(Region)',
                         config=Config(s3={'addressing_style': 'path'}))
         s3.upload_file(
-            '/tmp/org.json', bucket, "organisation-data/org.json") #uploading the file with the data to s3
+            '/tmp/org.csv', bucket, "organisation-data/org.csv") #uploading the file with the data to s3
         print("org data in s3")
     except Exception as e:
         print(e)
---
+++ static/Cost/300_Organization_Data_CUR_Connection/Code/org_data_ou.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+import argparse
+import boto3
+from botocore.exceptions import ClientError
+from botocore.client import Config
+import os
+
+def lambda_handler(event, context):
+    
+    # sts_connection = boto3.client('sts')
+    # acct_b = sts_connection.assume_role(
+    #     RoleArn="arn:aws:iam::(account id):role/OrganizationLambdaAccessRole",
+    #     RoleSessionName="cross_acct_lambda"
+    # )
+
+    # ACCESS_KEY = acct_b['Credentials']['AccessKeyId']
+    # SECRET_KEY = acct_b['Credentials']['SecretAccessKey']
+    # SESSION_TOKEN = acct_b['Credentials']['SessionToken']
+
+    # create service client using the assumed role credentials
+    client = boto3.client(
+        "organizations", region_name="us-east-1", #Using the Organizations client to get the data. This MUST be us-east-1 regardless of region you have the Lamda in
+        #aws_access_key_id=ACCESS_KEY,
+        #aws_secret_access_key=SECRET_KEY,
+        #aws_session_token=SESSION_TOKEN,
+    )
+
+
+    root_id    = client.list_roots()['Roots'][0]['Id']
+    ou_id_list = get_ou_ids(root_id, 'ORGANIZATIONAL_UNIT', client)
+    
+    with open('/tmp/org.csv', 'w') as f: # Saving in the temporay folder in the lambda
+        for ou in ou_id_list:
+
+            account_id_list = get_ou_ids(ou, 'ACCOUNT', client)
+            for account_id in account_id_list:
+                response = client.describe_account(AccountId=account_id)
+                aid = response["Account"]["Id"]
+                name = response["Account"]["Name"]
+                time = response["Account"]["JoinedTimestamp"]
+                status = response["Account"]["Status"]
+
+                line = f"{aid}, {name}, {time}, {status}, {ou}\n"
+                import pdb; pdb.set_trace()
+                f.write(line)
+            print("respose gathered")
+
+def s3_upload():
+    bucket = os.environ["BUCKET_NAME"] #Using environment variables below the Lambda will use your S3 bucket
+    try:
+        s3 = boto3.client('s3', 'eu-west-1',
+                        config=Config(s3={'addressing_style': 'path'}))
+        s3.upload_file(
+            '/tmp/org.csv', bucket, "organisation-data/org.csv") #uploading the file with the data to s3
+        print("org data in s3")
+    except Exception as e:
+        print(e)
+
+
+
+def get_ou_ids(parent_id, ChildType, client):
+  full_result = []
+  
+  paginator = client.get_paginator('list_children')
+  iterator  = paginator.paginate(
+    ParentId=parent_id,
+    ChildType= ChildType
+  )
+
+  for page in iterator:
+    for ou in page['Children']:
+      # 1. Add entry
+      # 2. Fetch children recursively
+      print(ou['Id'])
+      full_result.append(ou['Id'])
+      #full_result.extend(get_ou_ids(ou['Id']))
+
+  return full_result
+
+lambda_handler(None, None)
