e25ec35afad49111b7f241ccbc3010a014ece663
csaws <csaws@amazon.com>
2021-07-21 11:02:23 -0400

    modified annotations

--- static/Cost/300_CUR_Queries/Code/Database/rds_outposts.sql
+++ static/Cost/300_CUR_Queries/Code/Database/rds_outposts.sql
@@ -17,7 +17,7 @@ SELECT  -- automation_select_stmt
   SUM(CAST(line_item_unblended_cost AS decimal(16,8))) AS sum_line_item_unblended_cost 
 FROM  -- automation_from_stmt
   ${table_name} -- automation_tablename
-WHERE
+WHERE -- automation_where_stmt
   ${date_filter} -- automation_timerange_year_month
   AND product_location_type='AWS Outposts'
   AND product_product_family='Database Instance'
@@ -26,7 +26,7 @@ WHERE
     OR (line_item_line_item_type = 'SavingsPlanCoveredUsage')
     OR (line_item_line_item_type = 'DiscountedUsage')
   )
-GROUP BY
+GROUP BY -- automation_groupby_stmt
   DATE_FORMAT((line_item_usage_start_date),'%Y-%m-%d'), -- automation_timerange_dateformat
   bill_payer_account_id, 
   line_item_usage_account_id,
@@ -34,7 +34,8 @@ GROUP BY
   product_instance_type,
   product_database_engine,
   product_deployment_option
-ORDER BY
+ORDER BY -- automation_order_stmt
   day_line_item_usage_start_date ASC,
   sum_line_item_usage_amount DESC,
-  sum_line_item_unblended_cost DESC;
+  sum_line_item_unblended_cost DESC
+;
