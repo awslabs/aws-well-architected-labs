c6e5237b7cc62c8af187975b60a83a7a3bc6e729
Duncan Bell <duncbell@amazon.com>
2022-08-16 12:17:51 +0100

    Remove server folder incorrectly added

--- server/Reliability/300_Testing_for_Resiliency_of_EC2_RDS_and_S3/Code/FailureSimulations/c#/AppResiliency/build.ps1
+++
@@ -1,8 +0,0 @@
-dotnet restore
-
-dotnet publish --configuration Release --framework netcoreapp3.1 --runtime linux-arm64 --output ./output/linux-arm64 --self-contained true -p:PublishSingleFile=true -p:PlatformTarget=ARM64
-dotnet publish --configuration Release --framework netcoreapp3.1 --runtime linux-x64 --output ./output/linux-x64 --self-contained true -p:PublishSingleFile=true
-dotnet publish --configuration Release --framework netcoreapp3.1 --runtime win10-x64 --output ./output/win10-x64 --self-contained true -p:PublishSingleFile=true
-dotnet publish --configuration Release --framework netcoreapp3.1 --runtime osx-x64 --output ./output/osx-x64 --self-contained true -p:PublishSingleFile=true
-
-Compress-Archive -Path ./output/* -DestinationPath "csharpresiliency.zip" -Force
--- server/Security/300_Autonomous_Patching_With_EC2_Image_Builder_and_Systems_Manager/Code/scripts/watchscript.sh
+++
@@ -1,3 +0,0 @@
-while sleep 1; 
-do curl -s -o /dev/null -w "%{url_effective}, %{response_code}, %{time_total}\n" $1 ;
-done
