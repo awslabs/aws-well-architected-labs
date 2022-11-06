5d193056314026eb7243d853b4745e4482d03873
Iakov Gan <iakov@amazon.fr>
2022-11-06 10:02:17 +0100

    more refactoring

--- static/Cost/300_Optimization_Data_Collection/Code/source/ta/main.py
+++ static/Cost/300_Optimization_Data_Collection/Code/source/ta/main.py
@@ -13,23 +13,22 @@ role_name = os.environ['ROLENAME']
 crawler = os.environ["CRAWLER_NAME"]
 costonly = os.environ['COSTONLY'].lower() == 'yes'
 
-TEMP = "/tmp/data.json"
-
 def lambda_handler(event, context):
     print(json.dumps(event))
+    f_name = "/tmp/data.json"
     for r in event.get('Records', []):
         try:
             body = json.loads(r["body"])
             account_id = body["account_id"]
             account_name = body["account_name"]
-            read_ta(account_id, account_name)
-            upload_to_s3(prefix, account_id, body.get("payer_id"))
-            start_crawler()
+            read_ta(account_id, account_name, f_name)
+            upload_to_s3(prefix, account_id, body.get("payer_id"), f_name)
+            start_crawler(crawler)
         except Exception as e:
             print(f"{type(e)}: {e}")
 
-def upload_to_s3(prefix, account_id, payer_id):
-    if os.path.getsize(TEMP) == 0:
+def upload_to_s3(prefix, account_id, payer_id, f_name):
+    if os.path.getsize(f_name) == 0:
         print(f"No data in file for {prefix}")
         return
     d = datetime.now()
@@ -39,41 +38,37 @@ def upload_to_s3(prefix, account_id, payer_id):
     path = f"optics-data-collector/{prefix}-data/payer_id={payer_id}/year={year}/month={month}/{prefix}-{account_id}-{_date}.json"
     try:
         s3 = boto3.client("s3", config=Config(s3={"addressing_style": "path"}))
-        s3.upload_file(TEMP, bucket, path )
+        s3.upload_file(f_name, bucket, path )
         print(f"Data for {account_id} in s3 - {path}")
     except Exception as e:
         print(f"{type(e)}: {e}")
 
-def assume_role(account_id, service, region):
-    try:
-        assumed = boto3.client('sts').assume_role(RoleArn=f"arn:aws:iam::{account_id}:role/{role_name}", RoleSessionName=prefix)
-        creds = assumed['Credentials']
-        return boto3.client(service, region_name=region,
-            aws_access_key_id=creds['AccessKeyId'],
-            aws_secret_access_key=creds['SecretAccessKey'],
-            aws_session_token=creds['SessionToken'],
-        )
-    except ClientError as e:
-        print(f"{account_id} {type(e)}: {e}")
+def assume_role(account_id, service, region, role):
+    assumed = boto3.client('sts').assume_role(RoleArn=f"arn:aws:iam::{account_id}:role/{role}", RoleSessionName='--')
+    creds = assumed['Credentials']
+    return boto3.client(service, region_name=region,
+        aws_access_key_id=creds['AccessKeyId'],
+        aws_secret_access_key=creds['SecretAccessKey'],
+        aws_session_token=creds['SessionToken'],
+    )
 
-def start_crawler():
+def start_crawler(crawler):
     glue = boto3.client("glue")
     try:
         glue.start_crawler(Name=crawler)
     except Exception as e:
         if ('has already started' in str(e)):
-            print('crawler already started')
-            return
-        print(f"{type(e)}: {e}")
+            print('crawler has already started')
+        else:
+            print(f"{type(e)}: {e}")
 
-class DateTimeEncoder(JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, (date, datetime)):
-            return obj.isoformat()
+def _json_serial(self, obj):
+    if isinstance(obj, (datetime, date)): return obj.isoformat()
+    return JSONEncoder.default(self, obj)
 
-def read_ta(account_id, account_name):
-    f = open(TEMP, "w")
-    support = assume_role(account_id, "support", "us-east-1")
+def read_ta(account_id, account_name, f_name):
+    f = open(f_name, "w")
+    support = assume_role(account_id, "support", "us-east-1", role_name)
     checks = support.describe_trusted_advisor_checks(language="en")["checks"]
     for check in checks:
         #print(json.dumps(check))
@@ -93,6 +88,6 @@ def read_ta(account_id, account_name):
                 resource["Status"] = resource.pop("status") if "status" in resource else '-'
                 output.update({"AccountId":account_id, "AccountName":account_name, "Category": check["category"], 'DateTime': dt, 'Timestamp': ts, "CheckName": check["name"], "CheckId": check["id"]})
                 output.update(resource)
-                f.write(json.dumps(output, cls=DateTimeEncoder) + "\n")
+                f.write(json.dumps(output, default=_json_serial) + "\n")
         except Exception as e:
             print(f'{type(e)}: {e}')
