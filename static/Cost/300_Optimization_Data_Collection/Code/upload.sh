5c565a5133a90e5f7c5e3baef2c04cd2cab08251
Iakov Gan <iakov@amazon.fr>
2022-11-13 19:41:12 +0100

    doc and lint

--- static/Cost/300_Optimization_Data_Collection/Code/upload.sh
+++ static/Cost/300_Optimization_Data_Collection/Code/upload.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This script uploads CloudFormation files to S3 bucket. Can be used with any testing bucket or prod.
-# see also WA_s3_process.txt
+# see also README.md
 
 if [ -n "$1" ]; then
   bucket=$1
@@ -13,23 +13,12 @@ fi
 folder=$(pwd)
 code_path=$(git rev-parse --show-toplevel)/static/Cost/300_Optimization_Data_Collection/Code
 
-RED='\033[0;31m'
-BLUE='\034[0;31m'
-YELLOW='\033[0;33m'
-END='\033[0m' # No Color
-
-echo "linting CFN templates" 
-for template in $code_path/*.yaml; do
-  rel_path=$(realpath $template --relative-to="$(pwd)")
-  echo "Linting $rel_path"
-  echo "$(cfn-lint  --ignore-checks W3005 -- $rel_path && echo ' OK')" | awk '{ print "\t" $0 }'
-done
 
 echo "Building zips"
 
 for mod in fof ecs ta
 do
-  echo Buidling $mod
+  echo Buidling $mod.zip
   cd $code_path/source
   rm -f $mod.zip
   cd $code_path/source/$mod
@@ -39,10 +28,9 @@ done
 
 # FIXME: zips are allways recreated so allways uploaded. We can check if md5 is the same as before to avoid updates.
 
-echo 'Sync to $bucket'
+echo "Sync to $bucket"
 aws s3 sync $code_path/       s3://$bucket/Cost/Labs/300_Optimization_Data_Collection/ --exclude='*' --include='*.yaml' --acl public-read
 aws s3 sync $code_path/source s3://$bucket/Cost/Labs/300_Optimization_Data_Collection/ --exclude='*' --include='*.zip' --acl public-read
 aws s3 sync $code_path/source s3://$bucket/Cost/Labs/300_Optimization_Data_Collection/Region/ --exclude='*' --include='regions.csv' --acl public-read
 
 cd $pwd
-
