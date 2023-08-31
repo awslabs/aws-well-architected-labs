7e46987caa062d1aca34f0945cfa1ff979078821
Davide <davidecoccia@gmail.com>
2023-08-31 09:58:51 +0200

    updated the query to correctly consider current costs

--- static/Cost/300_CUR_Queries/Code/Networking_&_Content_Delivery/public-ipv4-costs.sql
+++ static/Cost/300_CUR_Queries/Code/Networking_&_Content_Delivery/public-ipv4-costs.sql
@@ -6,13 +6,14 @@ SELECT
 	"product_region", 
 	--"line_item_resource_id",
 	sum("line_item_usage_amount") usage_hours, 
-	sum("line_item_unblended_cost") actual_cost_usd,
-	sum("line_item_usage_amount")*0.005 projected_cost_usd
+	sum("line_item_unblended_cost") current_cost_usd,
+	SUM(IF (product_group = 'VPCPublicIPv4Address', line_item_usage_amount * 0.005, 0.0)) projected_cost_usd
 FROM ${table_name}
 WHERE 
 	year = cast(year(date_trunc('month', current_date) - interval '1' month) as varchar) 
 	and month in (date_format(date_trunc('month', current_date) - interval '1' month,'%m'),date_format(date_trunc('month', current_date) - interval '1' month,'%c'))
-	and "line_item_usage_type" like '%PublicIPv4%'
+	AND (line_item_usage_type LIKE '%PublicIPv4:%'
+    OR line_item_usage_type LIKE '%ElasticIP%')
 	and "line_item_line_item_type" = 'Usage'
 GROUP BY
 	date_trunc('day',"line_item_usage_start_date"), 
